8d3b123ccf604fd8f3443bf9a5ad22f2a53036ce
Rajan <rajanj448@student.sfit.ac.in>
2024-11-02 14:25:22 +0530

    Layout structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 
 import { Navigate, Route, Routes } from 'react-router-dom'
+import Layout from './layouts/layout'
+import Homepage from './pages/Homepage'
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<span>Home</span>} />
+      <Route path='/' element={<Layout>
+        <Homepage />
+      </Layout>} />
       <Route path='/user-profile' element={<span>Profile Page</span>} />
       <Route path='*' element={<Navigate to={"/"} />} />
     </Routes>
